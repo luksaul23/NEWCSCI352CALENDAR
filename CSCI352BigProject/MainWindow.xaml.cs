b7afb5ed3c5e9e841e7fbf68256502eec25bb181
Lukas Saul <luksaul@ut.utm.edu>
2018-10-18 20:06:24 -0500

    started to create structure for calendar

--- CSCI352BigProject/MainWindow.xaml.cs
+++ CSCI352BigProject/MainWindow.xaml.cs
@@ -23,6 +23,54 @@ namespace CSCI352BigProject
         public MainWindow()
         {
             InitializeComponent();
+
+            //populate grid with boxes
+            ColumnDefinition col0 = new ColumnDefinition();
+            ColumnDefinition col1 = new ColumnDefinition();
+            ColumnDefinition col2 = new ColumnDefinition();
+            ColumnDefinition col3 = new ColumnDefinition();
+            ColumnDefinition col4 = new ColumnDefinition();
+            ColumnDefinition col5 = new ColumnDefinition();
+            ColumnDefinition col6 = new ColumnDefinition();
+
+            primaryCalendar.ColumnDefinitions.Add(col0);
+            primaryCalendar.ColumnDefinitions.Add(col1);
+            primaryCalendar.ColumnDefinitions.Add(col2);
+            primaryCalendar.ColumnDefinitions.Add(col3);
+            primaryCalendar.ColumnDefinitions.Add(col4);
+            primaryCalendar.ColumnDefinitions.Add(col5);
+            primaryCalendar.ColumnDefinitions.Add(col6);
+
+            RowDefinition row0 = new RowDefinition();
+            RowDefinition row1 = new RowDefinition();
+            RowDefinition row2 = new RowDefinition();
+            RowDefinition row3 = new RowDefinition();
+            RowDefinition row4 = new RowDefinition();
+
+            primaryCalendar.RowDefinitions.Add(row0);
+            primaryCalendar.RowDefinitions.Add(row1);
+            primaryCalendar.RowDefinitions.Add(row2);
+            primaryCalendar.RowDefinitions.Add(row3);
+            primaryCalendar.RowDefinitions.Add(row4);
+
+            
+
+            //row0.Style{
+               // Border gridBorder = new Border()
+              //  {
+               //     BorderThickness = new Thickness()
+               //    {
+                //        Bottom = 1,
+                //        Left = 1,
+                //        Right = 1,
+                //        Top = 1
+               //     },
+                //    BorderBrush = new SolidColorBrush(Colors.Black)
+               // };
+           // };
+
+            primaryCalendar.Background = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 50, 100, 0));
+
         }
     }
 }
