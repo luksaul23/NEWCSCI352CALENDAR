f36c2d114c4988e36a7962ce8b44f7bc4608d15b
adachis <adachis>
2018-11-29 10:35:26 -0600

    Removed useless comments

--- CSCI352BigProject/MainWindow.xaml.cs
+++ CSCI352BigProject/MainWindow.xaml.cs
@@ -163,7 +163,7 @@ namespace CSCI352BigProject
         private void addEventButton_Click(object sender, RoutedEventArgs e)
         {
             string query = "select * from Events";
-            //OleDbCommand cmd = new OleDbCommand(query, cn);
+
             String date = dateBox.Text;
             String ev = eventBox.Text;
             
@@ -177,30 +177,7 @@ namespace CSCI352BigProject
             cmd.Parameters.Add("@Event", OleDbType.VarChar).Value = ev;
 
             cmd.ExecuteNonQuery();
-            /*
-            using (OleDbConnection myCon = new OleDbConnection(ConfigurationManager.ConnectionStrings["DbConn"].ToString()))
-            {
-                OleDbCommand cmd = new OleDbCommand();
-                cmd.CommandType = CommandType.Text;
-                cmd.CommandText = "insert into Items ([Item_Name],[Item_Price]) values (?,?);
-   cmd.Parameters.AddWithValue("@item", itemNameTBox.Text);
-                cmd.Parameters.AddWithValue("@price", Convert.ToDouble(itemPriceTBox.Text));
-                cmd.Connection = myCon;
-                myCon.Open();
-                cmd.ExecuteNonQuery();
-                System.Windows.Forms.MessageBox.Show("An Item has been successfully added", "Caption", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-            }
-            */
-
-            /*
-            OleDbDataReader read = cmd.ExecuteReader();
-            while (read.Read())
-            {
-                data += read[1].ToString() + " " + read[2].ToString() + "\n";
-            }
-
-            textBox1.Text = data;
-            */
+ 
 
             cn.Close();
 
